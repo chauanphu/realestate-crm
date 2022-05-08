7819c867e36aaecf8f0af9cd4c931e8b2fd07f96
chauanphu <chauanphu@gmail.com>
2022-05-08 09:28:24 +0700

    set up db

---
+++ src/knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: "./customers.sqlite3",
+    },
+    useNullAsDefault: true,
+  },
+
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./customers.sqlite3",
+    },
+  },
+};
---
+++ src/migrations/20220508022620_create_tables.js
@@ -0,0 +1,35 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("customers", (tbl) => {
+      tbl.increments(); // id
+      tbl.string("name", 50).notNullable();
+      tbl.string("email", 20);
+      tbl.string("phone", 15);
+      tbl.smallint("age");
+    })
+    .createTable("events", (tbl) => {
+      tbl.increments(); // id
+      tbl.date("date").notNullable();
+      tbl.text("content").notNullable();
+      tbl.text("progress").nullable();
+      // Foreign key
+      tbl
+        .integer("customer_id")
+        .references("id")
+        .inTable("customers")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("customers").dropTableIfExists("events");
+};
