0068771fab0e5a2dd94a838f7a2eacc4afd94115
chauanphu <chauanphu@gmail.com>
2022-05-09 11:03:26 +0700

    set up for deployment

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,18 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       nodeIntegration: true,
+      builderOptions: {
+        appId: "com.electron.realestate",
+        productName: "Customer Manager 4.0",
+        executableName: "Customer Manager 4.0",
+        extraResources: [{ from: "src/db", to: "./", filter: "**/*.sqlite3" }],
+        linux: {
+          target: "deb",
+          icon: "bundled/favicon.ico",
+          publish: ["github"],
+          category: "Office",
+        },
+      },
     },
   },
 };
