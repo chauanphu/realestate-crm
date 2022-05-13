3ec6e2a17f1b10119b00df1019a62b3e078f9d95
Phu Chau <chauanphu@gmail.com>
2022-05-13 07:40:35 +0700

    update new migration

--- src/knexfile.js
+++ src/knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./customers.sqlite3",
+      filename: "./db/_customers_.sqlite3",
     },
     useNullAsDefault: true,
   },
